ff2caafad1b166bcdbb943a7ac4214359f2af29b
danieltwentynine <danielmcardoso2016@protonmail.com>
2024-11-19 17:45:00 -0300

    adicionando o layout

---
+++ src/components/Digits.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const props = defineProps([''])
+</script>
+
+<template>
+    <div class="text-center mt-5">
+        <input type="number" class="p-2">
+        <input type="number" class="p-2 ms-2">
+    </div>
+    <div class="text-center mt-4">
+        <select name="" id="" class="p-1">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+    </div>
+    <div class="text-center mt-4">
+        <button type="submit" class="btn btn-dark p-2">Calculate</button>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-center mt-4">Simple Calculator</h1>
+    <h6 class="text-center">Created by Daniel Cardoso</h6>
+</template>
